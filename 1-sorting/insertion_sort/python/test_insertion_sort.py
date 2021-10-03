127d53b88379c2066f030dbed959ceb5e7461046
hazharaziz <hazharaziz5555@gmail.com>
2021-10-03 23:13:17 +0330

    update python tests for insertion sort algorithm

--- 1-sorting/insertion_sort/python/test_insertion_sort.py
+++ 1-sorting/insertion_sort/python/test_insertion_sort.py
@@ -4,6 +4,12 @@ from insertion_sort import *
 
 class TestInsertionSort(unittest.TestCase):
 
+    def test_empty_list(self):
+        numbers = []
+        insertion_sort(numbers)
+        self.assertIsNotNone(numbers)
+        self.assertEqual(0, len(numbers))
+
     def test_single_element_list(self):
         numbers = [1]
         insertion_sort(numbers)
@@ -12,11 +18,18 @@ class TestInsertionSort(unittest.TestCase):
         self.assertEqual(1, numbers[0])
 
     def test_list_of_numbers(self):
-        numbers = [4, 2, 5, 1]
-        insertion_sort(numbers)
-        self.assertIsNotNone(numbers)
-        self.assertEqual(4, len(numbers))
-        self.assertListEqual([1, 2, 4, 5], numbers)
+        numbers_1 = [4, 2, 5, 1]
+        numbers_2 = [31, 41, 59, 26, 41, 58]
+        
+        insertion_sort(numbers_1, False)
+        self.assertIsNotNone(numbers_1)
+        self.assertEqual(4, len(numbers_1))
+        self.assertListEqual([5, 4, 2, 1], numbers_1)
+
+        insertion_sort(numbers_2)
+        self.assertIsNotNone(numbers_2)
+        self.assertEqual(6, len(numbers_2))
+        self.assertListEqual([26, 31, 41, 41, 58, 59], numbers_2)
 
 
 if __name__ == '__main__':
